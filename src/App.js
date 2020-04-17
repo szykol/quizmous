bf12a01bde16eb4bdafc0b46777cd1f3f38ac217
szykol <szymonkolton@gmail.com>
2020-04-18 01:00:31 +0200

    Add login component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { render } from '@testing-library/react';
 import TitleBar from './components/TitleBar'
 import ApiVersion from './components/ApiVersion'
 import wrap_payload from './utils/jwt';
+import Login from './components/Login'
 
 class App extends React.Component {
   render = () => {
@@ -21,9 +22,7 @@ class App extends React.Component {
       </header>
       <div className="container col-md-6">
           <ApiVersion />
-          <button className="btn btn-primary">
-            Try it
-          </button>
+          <Login/>
       </div>
     </div>
   );
