29c9fccf97e572eddef58aba93ed103237e64c3d
szykol <szymonkolton@gmail.com>
2020-04-22 13:11:50 +0200

    Fix material ui components

--- src/components/Question.js
+++ src/components/Question.js
@@ -3,18 +3,92 @@ import apiRequest from '../utils/request';
 import Answer from './Answer';
 import YesNo from './YesNo';
 
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
+import { makeStyles } from '@material-ui/core/styles';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import Checkbox from '@material-ui/core/Checkbox';
+import InputLabel from '@material-ui/core/InputLabel';
+import TextField from '@material-ui/core/TextField';
+
+// const useStyles = makeStyles((theme) => ({
+//     root: {
+//       display: 'flex',
+//     },
+//     formControl: {
+//       margin: theme.spacing(3),
+//     },
+//   }));
+  
+
 export default function Question(props) {
     const question = props.data;
+    // const classes = useStyles();
     return (
-        <div className="card">
-            <div className="card-body">
-                <h5 className="card-title">{question.question}</h5>
-                { question.type == 'OPEN' ? <p><input type="text"></input></p> : null }
-                { question.type == 'YES_NO' ? <YesNo></YesNo> : null } 
-                <form className="controls">
-                    { question.answers.map((answer, idx) => <Answer data={answer} type={question.type} key={idx}></Answer>) }
-                </form>
-            </div>
-        </div>
+        <div style={{width: 300}}>
+        {question.type == "RADIO" && 
+        <FormControl fullWidth component="fieldset">
+        <FormLabel component="legend">{question.question}</FormLabel>
+        <RadioGroup aria-label="answer" name="answer">
+        { question.answers.map((answer, idx) => <FormControlLabel key={idx} value={answer.answer} control={<Radio />} label={answer.answer} />
+        ) }
+        </RadioGroup>
+        </FormControl>
+}
+        {
+        question.type == "YES_NO" && 
+        <FormControl fullWidth component="fieldset">
+        <FormLabel component="legend">{question.question}</FormLabel>
+        <RadioGroup aria-label="answer" name="answer">
+        { ["Yes", "No"].map((answer, idx) => <FormControlLabel key={idx} value={answer} control={<Radio />} label={answer} />
+        ) }
+        </RadioGroup>
+        </FormControl>
+}
+
+        {
+            question.type == "CHOICE" &&
+            <FormControl fullWidth component="fieldset">
+        <FormLabel component="legend">{question.question}</FormLabel>
+        <FormGroup>
+            {question.answers.map((answer, idx) => 
+          <FormControlLabel
+          control={<Checkbox key={idx} name={answer.answer} />}
+          label={answer.answer}
+          />
+          )}
+        </FormGroup>
+      </FormControl>
+        }
+
+{
+            question.type == "OPEN" &&
+              <TextField
+                variant="outlined"
+                name="password"
+                label={question.question}
+                multiline
+            />
+        }
+
+    </div>
+
+
+
+        // <div className="card">
+        //     <div className="card-body">
+        //         <h5 className="card-title">{question.question}</h5>
+        //         { question.type == 'OPEN' ? <p><input type="text"></input></p> : null }
+                
+        //         { question.type == 'YES_NO' ? <YesNo></YesNo> : null } 
+        //         <form className="controls">
+        //             { question.answers.map((answer, idx) => <Answer data={answer} type={question.type} key={idx}></Answer>) }
+        //         </form>
+        //     </div>
+        // </div>
     )
 }
--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import apiRequest from '../utils/request';
 import Question from './Question';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+
+
 
 export default function Quiz() {
     const [quiz, setQuiz] = useState({
@@ -19,15 +23,28 @@ export default function Quiz() {
     }, []);
 
     return (
-        <div className="container-sm">
+        <div>
             <h2>{quiz.name}</h2>
             <h3>{quiz.description}</h3>
+            <Grid
+                container
+                spacing={5}
+                direction="column"
+                alignItems="flex-start"
+                justify="center"
+                >
+                {quiz.questions.map((question, idx) => 
+                <Grid container alignItems="flex-start" style={{minWidth: 500}}
+                justify="center" key={idx} alignContent="flex-start" item xs={12}>
+                    <Question data={question} ></Question>
+                </Grid>
+                )}
+            </Grid>
 
-            {quiz.questions.map((question, idx) => <Question data={question} key={idx}></Question>)}
-
-            <button className="btn btn-primary">
+            
+            <Button variant="contained" color="primary">
                 Finish
-            </button>
+            </Button>
         </div>
     )
 }
